6956098354da0302e1a8d3c98e6e0eedd642456b
Samantha Derochie <Samantha Derochie>
2017-07-30 19:03:21 -0700

    add exit sound

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Exit : MonoBehaviour {
+    
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
